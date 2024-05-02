ca57e233c651f1ec1513700cf128e43b881e4180
Vladimir23 <vladimyr.yes@gmail.com>
2024-05-02 11:09:55 +0100

    moved into the app/ subfolder

--- app/st_pred.py
+++ app/st_pred.py
@@ -7,6 +7,10 @@ import shutil
 import torch
 from ultralytics import YOLO
 
+CUR_WORKDIR = os.getcwd()
+PRED_DIR = os.path.join(CUR_WORKDIR, 'predict')
+MODEL_PATH = os.path.join(CUR_WORKDIR, 'models/best.pt')
+MODEL = YOLO(MODEL_PATH)
 
 # function to clean the folder
 def clean_folder(folder_path):
@@ -20,20 +24,20 @@ def clean_folder(folder_path):
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
 
-# Function to perform prediction (Replace with your actual prediction logic)
+
+# Function to perform prediction on one image (Replace with your actual prediction logic)
 def perform_prediction(image):
-    model = YOLO('models/best.pt')
     results = []
     # clean the prediction destination folder
-    clean_folder("predict/")
+    clean_folder(PRED_DIR)
     with torch.no_grad():
-        results = model.predict(image, conf=0.4, iou=0.08, save=True, project="predict", name="image", device='cpu')
+        results = MODEL.predict(image, conf=0.4, iou=0.08, save=True, project="predict", name="images", device='cpu')
 
     return results
 
+
 def main():
     st.title("Image Upload and Prediction")
-
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
@@ -47,9 +51,16 @@ def main():
         st.image(image, channels="BGR", caption="Uploaded Image", use_column_width=True)
 
         if st.button("Predict"):
-            prediction = perform_prediction(image)
             # File path for the image
-            image_path = "predict/image/image0.jpg"
+            image_dir = os.path.join(PRED_DIR, 'images')
+            image_path = os.path.join(image_dir, "image0.jpg")
+            if not os.path.exists(image_dir):
+                os.makedirs(image_dir)
+            else:
+                clean_folder(image_dir)
+
+            prediction = perform_prediction(image)
+
             try:
                 # Read the image using OpenCV
                 image = cv2.imread(image_path)
@@ -64,6 +75,7 @@ def main():
                 st.error(f"Error: {e}")
 
             st.success(f"Prediction: {prediction}")
+
 
 if __name__ == "__main__":
     main()
